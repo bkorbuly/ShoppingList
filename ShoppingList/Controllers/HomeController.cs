f5707b3905ec5a27222af8a73c4eed73a5c20ef6
bkorbuly <b.korbuly@gmail.com>
2017-11-13 15:55:19 +0100

    New Repository made

---
+++ ShoppingList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingList.Models;
+
+
+namespace ShoppingList.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var item = new Item() { Id = 1, ItemName = "soupcube", IsUrgent = true, Description = "for the soup", Quantity = 1 };
+            return View(item);
+        }
+    }
+}
---
+++ ShoppingList/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class User
+    {
+        public int Name{ get; set; }
+        public DateTime Date { get; set; }
+    }
+}
